ac0083bf15486fa63ddcfc9cc09c67ac55296773
dhanushkaka02 <dhanushkaka02@gmail.com>
2025-01-07 18:45:06 +0530

    hello

---
+++ src/main/java/com/example/myproject/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.myproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public RedirectView home() {
+        return new RedirectView("/home.html"); // Redirects to home.html in the static folder
+    }
+    
+}
+
